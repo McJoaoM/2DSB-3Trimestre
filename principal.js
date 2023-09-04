ee89fdcffdab32cabdd8ff15e7279580b4780fbd
McJoaoM <130366173+McJoaoM@users.noreply.github.com>
2023-09-04 11:48:53 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,10 @@
+
+//------------------------------------------------------------------
+//daqui pra baixo começa do js do site
+
+//busca no site pelo titulo e armazena ele dentro da variável titulo
+let titulo = document.querySelector(".titulo");
+//motra no devtools o conteudo de texto
+console.log(titulo.textContent);
+//altera o conteúdo da variável titulo para o texto desejado
+titulo.textContent = "Suco de whatsapp"
